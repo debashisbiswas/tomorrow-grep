7968540deea7bbeeb8ab377df91da06eed34fd85
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2026-01-04 12:51:25 -0700

    update flake to support darwin

--- flake.nix
+++ flake.nix
@@ -4,22 +4,37 @@
   outputs =
     { nixpkgs, ... }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      python = pkgs.python3.withPackages (ps: [
-        ps.feedparser
-        ps.beautifulsoup4
-        ps.markdownify
-      ]);
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = [
-          python
-          pkgs.whisper-cpp
-          pkgs.ffmpeg
-          pkgs.parallel
-          pkgs.curl
-        ];
-      };
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+          python = pkgs.python3.withPackages (ps: [
+            ps.feedparser
+            ps.beautifulsoup4
+            ps.markdownify
+          ]);
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              python
+              pkgs.whisper-cpp
+              pkgs.ffmpeg
+              pkgs.parallel
+              pkgs.curl
+            ];
+          };
+        }
+      );
     };
 }
